0a42540c3904c60fb9d2d745a627e45596eb07dd
sam_dev <samueldevpro09@gmail.com>
2022-09-29 16:46:18 +0200

    fix 3

--- functions/contact.js
+++ functions/contact.js
@@ -1,6 +1,41 @@
+const axios = require("axios");
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("success"),
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization:
+      "Bearer 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",
   };
+
+  axios
+    .post(
+      "https://api.sendgrid.com/v3/mail/send",
+      {
+        personalizations: [
+          {
+            to: [{ email: "samueldev09@gmail.com", name: "John Doe" }],
+            subject: "Hello, World!",
+          },
+        ],
+        content: [{ type: "text/plain", value: "Heya!" }],
+        from: { email: "ticklme@tickl.ch", name: "lfish" },
+        reply_to: { email: "ticklme@tickl.ch", name: "lfish reply" },
+      },
+      { headers: headers }
+    )
+    .then(function (response) {
+      console.log(response);
+      return {
+        statusCode: 200,
+        body: JSON.stringify("success"),
+      };
+    })
+    .catch(function (error) {
+      console.log(error);
+
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      };
+    });
 };
