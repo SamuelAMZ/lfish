1df6b91ce752e13c568437a02c6950dacafcbf73
sam_dev <samueldevpro09@gmail.com>
2022-10-04 20:04:32 +0200

    chnages 12

--- src/components/pageComp/PartenaireComps.jsx
+++ src/components/pageComp/PartenaireComps.jsx
@@ -3,7 +3,7 @@ import SectionAf from "../SectionAf";
 
 const textA = {
   heading: "Nos Partenaires",
-  body: "La majorité de nos partenaires sont au Togo: institutions et laboratoires, centre de formation dédiées à l'aquaculture, l'agriculture et la pêche. <br><br> Nos partenaires, ne sont pas d'autres personnes que vous, les fermes piscicoles, les représentants de la marques PM sont nos premiers partenaires. <br><br> Nous avons également des partenaires privés qui nous accompagnent dans la technique, la recherche et le développement, la communication. Le réseau est au coeur de la croissance économique et de l'inclusion financière.<br> <br> Plus de partenaires chez L'fish, mieux c'est!",
+  body: "La majorité de nos partenaires sont au Togo: institutions et laboratoires, centres de formation dédiées à l'aquaculture, l'agriculture et la pêche. <br><br> Nos partenaires, ne sont pas d'autres personnes que vous: les fermes piscicoles, les représentants de la marque PM sont nos premiers partenaires. <br><br> Nous avons également des partenaires privés qui nous accompagnent dans la technique, la recherche et le développement, la communication. Le réseau est au coeur de la croissance économique et de l'inclusion financière.<br> <br> Plus de partenaires chez L'fish, mieux c'est!",
 };
 
 const PartenaireComps = () => {
